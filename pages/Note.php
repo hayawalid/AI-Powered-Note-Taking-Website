1b92876e3a1be8a24487345962875af9bc81541f
Shahd Emad <134103174+shahdcode@users.noreply.github.com>
2024-10-20 10:31:28 +0300

    Added note/summarization

---
+++ pages/Note.php
@@ -0,0 +1,55 @@
+<?php include '../includes/user_sidebar.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/user_styles.css">
+    <link rel="stylesheet" href="../assets/css/Note.css">
+    <script src="../assets/js/sidebar.js"></script>
+    <script src="../assets/js/Note.js"></script>
+    <link rel='stylesheet' href='https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.css'><link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body class="Bootstrap-body">
+
+
+<div id="container-fluid">
+  <div class="row come-in">
+    <h1 class="text-center">Esm el folder w el note el mafto7a</h1>
+   
+    
+    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+      <div class="panel panel-info">
+        <div class="panel-heading">Artifical intelligence vs machine learning</div>
+        <div class="panel-body">
+          <p>Artificial intelligence and machine learning, what's the difference? 
+            Are they the same? Well, some people kind of frame the question this way. It's AI versus ML. 
+            Is that the right way to think of this or is it AI? Equals ML. Or is it AI is somehow something different than ML? 
+            Here's 3 equations. I wonder which one is going to be right. Well, let's talk about this. First of all, when we talk about AII, think it's important to come with definitions because a lot of people have different ideas of what this is. So I'm going to assert the simple definition that AI is basically exceeding or matching. The capabilities of a human. So we're trying to match the intelligence, whatever that means, and capabilities of a human subject. Now what could that involve? There's a number of different things. For instance, one of them is the ability to discover, to find out new information. Another is the ability to infer, to read in information from other sources that maybe has not been explicitly stated. And then also the ability to reason. The ability to figure things out. I put this and this together and I come up with something else. So I'm going to suggest to you this is what AI is, and that's the definition we'll use for this discussion. Now, what kinds of things then would be involved if we were talking about doing machine learning? Well, machine learning, I'm going to put that over here, is basically a capability. Start with a Venn diagram. Machine learning involves predictions or decisions based on data. Think about this as a very sophisticated form of statistical analysis. It's looking for predictions based upon information that we have. So the more we feed into the system, the more it's able to give us accurate predictions and decisions based upon that data. It's something that learns. That's the L part. Rather than having to be programmed when we program a system, I have to come up with all the code and if I wanted to do. Different. I have to go change the code and then get a different outcome. In the machine learning situation, what I'm doing could be adjusting some models, but it's different than programming and mostly it's learning the more data that I give to it. So it's based on large amounts of information and there's a couple of different fields within a couple of different types. There is supervised machine learning and as you might guess, there's an unsupervised machine learning. And the main difference, as the name implies, is one has more human oversight. Looking at the training of the data using labels that are superimposed on the data unsupervised is kind of able to run more and and find things that were not explicitly stated.</p>
+        </div>
+        <button class="summarize">Summerize</button>
+      </div>          
+    </div>
+    
+    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 summaried-class">
+      <div class="panel panel-warning">
+        <div class="panel-heading">Summarized Text</div>
+        <div class="panel-body">
+          <p>"Don't speak of it, I beg of you," replied the Woodman.  
+            "I have no heart, you know, so I am careful to help all those who may need a friend, 
+            even if it happens to be only a mouse."</p>
+
+      
+        </div>
+      </div> 
+      
+   
+    </div>
+  </div>
+</div>
+
+
+</div>
+</body>
+</html>
