e3c4c9839137ffb04d3d54cd5ae8e86e90cca7a6
hayawalid <hayawalid640@gmail.com>
2024-10-28 02:40:49 +0300

    Update 500.php

--- includes/500.php
+++ includes/500.php
@@ -1,85 +1,65 @@
+
+
+
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../assets/images/favicon.ico" />
-    <link rel="stylesheet" href="../assets/css/backend-plugin.min.css?v=1.0.0">
-    <link rel="stylesheet" href="../assets/css/backend.css?v=1.0.0">
-    <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
-    <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
-    <link rel="stylesheet" href="../assets/vendor/@icon/dripicons/dripicons.css">
-</head>
-
-<body class=" ">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title></title>
+      
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="../assets/images/favicon.ico" />
+      <link rel="stylesheet" href="../assets/css/backend-plugin.min.css?v=1.0.0">
+      <link rel="stylesheet" href="../assets/css/backend.css?v=1.0.0">
+      <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+      <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+      <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+      <link rel="stylesheet" href="../assets/vendor/@icon/dripicons/dripicons.css">  </head>
+  <body class=" ">
     <!-- loader Start -->
     <div id="loading">
-        <div id="loading-center">
-        </div>
+          <div id="loading-center">
+          </div>
     </div>
     <!-- loader END -->
-
-    <?php
-    session_start();
-    $userPages = null;
-
-    if (isset($_SESSION['UserID'])) {
-        $userID = $_SESSION['UserID'];
-        $user = User::find($userID);  // Assuming you have a method to get the user by ID
-        if ($user) {
-            $firstPage = $user->userType_obj->pages_array[0]->link_address;
-            $userPages = htmlspecialchars($firstPage);
-        } else {
-            $userPages = '../pages/UserDashboard.php';
-        }
-    } else {
-        $userPages = '../pages/UserDashboard.php';
-    }
-    ?>
-
-    <div class="wrapper">
-        <div class="container">
-            <div class="row no-gutters height-self-center">
-                <div class="col-sm-12 text-center align-self-center">
-                    <div class="iq-error position-relative">
-                        <img src="../assets/images/500.png" class="img-fluid iq-error-img" alt="">
-                        <h2 class="mb-0">Oops! You are not authorized.</h2>
-                        <p>Unauthorized Access.</p>
-                        <a class="btn btn-primary d-inline-flex align-items-center mt-3"
-                            href="<?php echo $userPages; ?>"><i class="ri-home-4-line"></i>Back to Home</a>
-                    </div>
-                </div>
+    
+      <div class="wrapper">
+      <div class="container">
+         <div class="row no-gutters height-self-center">
+            <div class="col-sm-12 text-center align-self-center">
+               <div class="iq-error position-relative">
+                     <img src="../assets/images/500.png" class="img-fluid iq-error-img" alt="">
+                     <h2 class="mb-0">Oops! You are not authorized.</h2>
+                     <p>Unauthorized Access.</p>
+                     <a class="btn btn-primary d-inline-flex align-items-center mt-3" href="../pages/UserDashboard.php"><i class="ri-home-4-line"></i>Back to Home</a>
+                     
+               </div>
             </div>
-        </div>
-    </div>
-
-
+         </div>
+   </div>
+      </div>
+    
     <!-- Backend Bundle JavaScript -->
     <script src="../assets/js/backend-bundle.min.js"></script>
-
+    
     <!-- Flextree Javascript-->
     <script src="../assets/js/flex-tree.min.js"></script>
     <script src="../assets/js/tree.js"></script>
-
+    
     <!-- Table Treeview JavaScript -->
     <script src="../assets/js/table-treeview.js"></script>
-
+    
     <!-- SweetAlert JavaScript -->
     <script src="../assets/js/sweetalert.js"></script>
-
+    
     <!-- Chart Custom JavaScript -->
     <script src="../assets/js/chart-custom.js"></script>
-
+    
     <!-- slider JavaScript -->
     <script src="../assets/js/slider.js"></script>
-
+    
     <!-- app JavaScript -->
     <script src="../assets/js/app.js"></script>
-</body>
-
+  </body>
 </html>
