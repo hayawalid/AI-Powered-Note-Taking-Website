0327ee86b3f08d0f971ebd6cf9c2dd979cc28e23
janaghoniem <jana2208912@miuegypt.edu.eg>
2024-11-22 23:04:39 +0200

    Update uploads.php

--- pages/uploads.php
+++ pages/uploads.php
@@ -1,3 +1,9 @@
+<?php
+    include_once '../includes/session.php'; 
+
+    //set current page to update sidebar status
+    $current_page = 'Upload File';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,12 +14,20 @@
     <link rel="stylesheet" href="../assets/css/uploads.css">
     <link rel="stylesheet" href="../assets/css/user_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- CSS Files -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/now-ui-dashboard.css" rel="stylesheet" />
+    <link href="../assets/css/demo.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">  
 </head>
 
 <body>
     <div class="container">
-        <?php include '../includes/user_sidebar.php'; ?>
+        <?php include '../includes/sidebar.php'; ?>
 
 
         <div class="containers">
@@ -59,8 +73,18 @@
         });
 
     </script>
-        <script src="../assets/js/sidebar.js"></script>
-
+    <script src="../assets/js/sidebar.js"></script>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/jquery.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!-- Chart JS -->
+    <script src="../assets/js/plugins/chartjs.min.js"></script>
+    <!--  Notifications Plugin    -->
+    <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
 </body>
 
 </html>
