179cb3ab1ec3f0791c54244ccebe0143c694cc32
janaghoniem <jana2208912@miuegypt.edu.eg>
2024-11-22 21:17:35 +0200

    sidebar update

--- includes/Page.php
+++ includes/Page.php
@@ -6,6 +6,7 @@ class Page {
     public $id;
     public $friendly_name;
     public $link_address;
+    public $link_icon;
 
     // function __construct($id) {
     //     if($id != "") {
@@ -26,6 +27,7 @@ class Page {
 				$this->friendly_name=$row2["friendly_name"];
 				$this->link_address=$row2["link_address"];
 				$this->id=$row2["id"];
+                $this->link_icon=$row2["link_icon"];
 			}
 		}
 	}
